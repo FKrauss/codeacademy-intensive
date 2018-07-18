c8a8cf9295373252cbda47744ae8f2dc06409049
fabio krauss <fks@fabios-MacBook-Pro.local>
2018-07-18 17:22:08 +0200

    started with FE course

---
+++ fe/projects/minesweeper/src/minersweeper.js
@@ -0,0 +1,18 @@
+const blankLine ='  |   |  ';
+const guessLine ='1 |   |  ';
+const bombLine = '  | B |  ';
+
+const testmessage = "This is what an empty board would look like:";
+const realmessage = "This is what a board with a guess and a bomb on it would look like:";
+
+console.log(testmessage);
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+
+console.log(realmessage);
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
+
